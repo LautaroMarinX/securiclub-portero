15b21ef11902334200968651efd867fbb76f56b0
XentraMarin <lautaro.marin@googlobal.com>
2025-10-30 11:31:37 -0300

    git ignore actualizado

--- .vercel/output/_functions/entry.mjs
+++ .vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_Bw35Y6Ch.mjs';
-import { manifest } from './manifest_B7yYU62Z.mjs';
+import { manifest } from './manifest_kKFmB9y0.mjs';
 
 const serverIslandMap = new Map();;
 
@@ -21,7 +21,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "c22050a8-bfca-4ab7-bd89-6e54e33802cb",
+    "middlewareSecret": "c9f84ffd-d127-4ee2-966f-c5bd3b5d3722",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
